a29f23bc402999d2424714a0ebfb9c35b4366103
Maddie Jennings <madisonlynnjennings@gmail.com>
2018-12-18 21:02:32 -0600

    made progress on list class

--- Day7/day7.js
+++ Day7/day7.js
@@ -1,50 +1,71 @@
 const fs = require('fs-extra')
 const path = require('path')
 
-class LinkedList {
+class Node {
+  constructor(value, next) {
+    this.value = value
+    this.next = [next]
+  }
+
+  addNext(value) {
+    this.next.push(value)
+  }
+
+}
+
+class instructionList {
   constructor() {
-    this.head = null
-    this.tail = null
+    this.list = []
   }
 
-  addToHead(value) {
-    const newNode = new Node(value, this.head, null)
-    if (this.head) this.head.prev = newNode
-    else this.tail = newNode
-    this.head = newNode
-  }
-
-  addToTail(value) {
-    const newNode = new Node(value, null, this.tail)
-    if (this.tail) this.tail.next = newNode
-    else this.head = newNode
-    this.tail = newNode
+  addNode(value, next) {
+    const newNode = new Node(value, next)
+    this.list.push(newNode)
+    return newNode
   }
 
   find(value) {
-    let thisNode = this.head
-    while (thisNode) {
-      if (thisNode.value === value) {
-        return thisNode
+    for (let i = 0; i < this.list.length; i++) {
+      if (this.list[i].value === value) {
+        return this.list[i]
       }
-
-      thisNode = thisNode.next
     }
-    return thisNode
   }
 }
 
-function Node(value, next, prev) {
-  this.value = value
-  this.next = next
-  this.prev = prev
+
+const parseInput = arr => {
+  let orderedList = []
+  let list = new instructionList()
+  for (let i = 0; i < arr.length; i++) {
+    const value = arr[i].split('Step ')[1].split(' must')[0]
+    const next = arr[i].split('step ')[1].split(' can')[0]
+    if (list.find(value)) {
+      let currentNode = list.find(value)
+      currentNode.addNext(next)
+    } else {
+      list.addNode(value, next)
+    }
+  }
+  for (let j = 0; j < list.list.length; j++) {
+    let nextList = list.list[j].next
+    if (nextList.length > 1) {
+      let sortedList = nextList.sort()
+      for (let k = 0; k < sortedList.length; k++) {
+        orderedList.push(sortedList[k])
+      }
+    } else {
+      orderedList.push(nextList[0])
+    }
+  }
+  console.log(orderedList)
 }
 
 const getInput = async () => {
   let input = await fs.readFile(path.join(__dirname, 'input.txt'), 'utf8')
   input = input.split('\n')
   input.pop()
-  findEdges(input)
+  parseInput(input)
 }
 
 getInput()
